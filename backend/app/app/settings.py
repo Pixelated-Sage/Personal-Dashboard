731ad00d3f935be0056ecb5feeef2675d08d9820
Pixelated-Sage <abhisheksharma.cse59@gmail.com>
2025-07-10 07:01:51 +0530

    changes

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z+)8fg(iqjo@m!7k&@q-d&ob6%r9z4w+e5pc6!dd-%7p(wf4)%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*", "http://localhost:5173/"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
@@ -53,8 +51,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True  # for development only
-
+CORS_ALLOWED_ORIGINS = [
+  "https://personal-dashboard-xi-ivory.vercel.app/",  # Your Vercel frontend URL
+]
 
 ROOT_URLCONF = 'app.urls'
 
